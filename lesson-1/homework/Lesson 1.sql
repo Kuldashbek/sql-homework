bba9558cd906bea29473eab7a2e10813cadd343f
Kuldashbek <kuldashbek@gmail.com>
2025-08-03 15:20:57 +0500

    Update Lesson 1.sql

--- lesson-1/homework/Lesson 1.sql
+++ lesson-1/homework/Lesson 1.sql
@@ -16,12 +16,12 @@
 --Windows Authentication: Использует учетную запись Windows.
 --SQL Server Authentication: Требует имя пользователя и пароль, определенные в SQL Server.
 
-Medium
+--Medium
 --4. Создание базы данных:
 
 CREATE DATABASE SchoolDB;
 
-5. Создание таблицы Students:
+--5. Создание таблицы Students:
 
 USE SchoolDB;
 
@@ -32,26 +32,27 @@ CREATE TABLE Students (
 );
 
 --6. Разница между SQL Server, SSMS и SQL:
-SQL Server	СУБД, где хранятся и управляются базы данных.
-SSMS SQL Server Management Studio — инструмент (GUI) для работы с SQL Server.
-SQL Язык запросов для управления данными (SELECT, INSERT и т.д.).
+--SQL Server	СУБД, где хранятся и управляются базы данных.
+--SSMS SQL Server Management Studio — инструмент (GUI) для работы с SQL Server.
+--SQL Язык запросов для управления данными (SELECT, INSERT и т.д.).
 
-Hard
+--Hard
 --7. Типы SQL-команд:
-Категория	Название	Примеры	Назначение
-Категория	Название	Примеры	Назначение
-DQL	Data Query Language	SELECT	Чтение данных
-DML	Data Manipulation Language	INSERT, UPDATE, DELETE	Работа с данными
-DDL	Data Definition Language	CREATE, ALTER, DROP	Работа со структурами
-DCL	Data Control Language	GRANT, REVOKE	Управление доступом
-TCL	Transaction Control Language	COMMIT, ROLLBACK, SAVEPOINT	Управление транзакциями
+--Категория	Название	Примеры	Назначение
+--Категория	Название	Примеры	Назначение
+--DQL	Data Query Language	SELECT	Чтение данных
+--DML	Data Manipulation Language	INSERT, UPDATE, DELETE	Работа с данными
+--DDL	Data Definition Language	CREATE, ALTER, DROP	Работа со структурами
+--DCL	Data Control Language	GRANT, REVOKE	Управление доступом
+--TCL	Transaction Control Language	COMMIT, ROLLBACK, SAVEPOINT	Управление транзакциями
 
-8. Вставка 3 записей в Students:
+--8. Вставка 3 записей в Students:
 
 INSERT INTO Students (StudentID, Name, Age)
 VALUES 
 (1, 'Ali', 20),
 (2, 'Diyorah', 19),
 (3, 'Mehribon', 21);
+
 
 select * from Students
